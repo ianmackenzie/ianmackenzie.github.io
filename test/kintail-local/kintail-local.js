a22685279bc9b12318c980b1e6c7f0d44a4591d3
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-04-09 14:46:57 -0400

    Switch to explicit initialization

--- test/kintail-local/kintail-local.js
+++ test/kintail-local/kintail-local.js
@@ -1,26 +1,33 @@
 if ('window' in self) {
-  console.log('Running as main script')
-  thisScript = document.currentScript.src
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistration().then(function(registration) {
-      if (registration === undefined) {
-        console.log('No existing service worker')
-      } else {
-        console.log('Unregistering existing service worker')
-        registration.unregister();
+    self['Kintail'] = {}
+    Kintail['Local'] = {}
+    Kintail.Local['init'] = (function(thisScript) {
+      function registerServiceWorker() {
+        console.log('Registering service worker')
+        return navigator.serviceWorker.register(thisScript).then(function(registration) {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+          console.log('ServiceWorker registration failed: ', err);
+        });
       }
-      console.log('Registering service worker')
-      navigator.serviceWorker.register(thisScript).then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(function(err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
+
+      return function() {
+        return navigator.serviceWorker.getRegistration().then(function(registration) {
+          if (registration === undefined) {
+            console.log('No existing service worker')
+            return registerServiceWorker()
+          } else {
+            console.log('Unregistering existing service worker')
+            registration.unregister().then(registerServiceWorker);
+          }
+        });
+      }
+    })(document.currentScript.src)
   } else {
     console.log('Service workers are not supported')
   }
 } else if (self instanceof ServiceWorkerGlobalScope) {
-  console.log('Running as service worker')
   self.addEventListener('fetch', function(event) {
     console.log('Intercepted fetch event for ' + event.request.url)
   });
