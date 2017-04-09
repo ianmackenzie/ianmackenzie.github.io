855d76921b52f05bacba12abb0f420382118cd5d
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-04-09 13:26:18 -0400

    Try explicit relative path

--- test/kintail-local/kintail-local.js
+++ test/kintail-local/kintail-local.js
@@ -2,7 +2,7 @@ if (self instanceof Window) {
   console.log('Running as main script')
   if ('serviceWorker' in navigator) {
     console.log('Registering service worker')
-    navigator.serviceWorker.register(document.currentScript.src, {scope: '//local'}).then(function(registration) {
+    navigator.serviceWorker.register(document.currentScript.src, {scope: './/local'}).then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function(err) {
       console.log('ServiceWorker registration failed: ', err);
