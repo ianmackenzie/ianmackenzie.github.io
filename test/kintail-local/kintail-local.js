cd8602e8e0ffaea85472e63d601c840d70793400
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-04-09 13:24:11 -0400

    Add kintail/local test page

---
+++ test/kintail-local/kintail-local.js
@@ -0,0 +1,18 @@
+if (self instanceof Window) {
+  console.log('Running as main script')
+  if ('serviceWorker' in navigator) {
+    console.log('Registering service worker')
+    navigator.serviceWorker.register(document.currentScript.src, {scope: '//local'}).then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  } else {
+    console.log('Service workers are not supported')
+  }
+} else if (self instanceof ServiceWorkerGlobalScope) {
+  console.log('Running as service worker')
+  self.addEventListener('fetch', function(event) {
+    console.log('Intercepting fetch event for ' + event.request.url)
+  });
+}
