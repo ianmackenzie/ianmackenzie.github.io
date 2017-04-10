99b4cc5a77e8ef5de5a0c1f2f873fd1408eb0eec
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-04-09 20:32:11 -0400

    Try to integrate with Elm code

--- test/kintail-local/kintail-local.js
+++ test/kintail-local/kintail-local.js
@@ -3,11 +3,67 @@ if ('window' in self) {
     self['Kintail'] = {}
     Kintail['Local'] = {}
     Kintail.Local['init'] = (function(thisFile) {
+      function badRequest(description) {
+        return Promise.resolve({status: 400, statusText: 'Bad Request', body: description})
+      }
+
+      function notFound(description) {
+        return Promise.resolve({status: 404, statusText: 'Not Found', body: description})
+      }
+
+      function text(body) {
+        return Promise.resolve({status: 200, statusText: 'OK', body: body})
+      }
+
+      function ok(body) {
+        return Promise.resolve({status: 200, statusText: 'OK', body: body})
+      }
+
+      function readFile(body) {
+        var requestedFile = JSON.parse(body)
+
+        var fileElement = document.getElementById(requestedFile.elementId)
+        if (fileElement === null) {
+          return notFound('Could not find <file> element')
+        }
+
+        var index = requestedFile.index
+        if (index < 0 || index >= fileElement.files.size) {
+          return notFound('Invalid index for given <file> element')
+        }        
+        
+        var file = fileElement.files[index]
+        if (file.name !== requestedFile.name) {
+          return notFound('File name does not match')
+        }
+        if (file.size !== requestedFile.size) {
+          return notFound('File size does not match')
+        }
+        if (file.lastModified !== requestedFile.lastModified) {
+          return notFound('File last-modified time does not match')
+        }
+        if (file.type !== requestedFile.mimeType) {
+          return notFound('File MIME type does not match')
+        }
+
+        return new Promise(function(resolve, reject) {
+          var reader = new FileReader();
+
+          reader.onload = function(event) {
+            resolve(reader.result)
+          }
+
+          reader.readAsText(file);
+        }).then(function(text) {
+          return ok(text)
+        })
+      }
+
       function handleRequest(request) {
-        if (request.method == 'GET' && request.path == 'file/read') {
-          return Promise.resolve({status: 200, statusText: 'OK', body: "<file contents>"})
+        if (request.path == 'file/read') {
+          return readFile(request.body)
         } else {
-          return Promise.resolve({status: 400, statusText: "Unrecognized request", body: ""})
+          return badRequest("Unrecognized request")
         }
       }
 
@@ -71,14 +127,12 @@ if ('window' in self) {
     var request = event.request
     var url = request.url
     if (url.startsWith('https://kintail/local/')) {
+      var path = url.slice(22)
       event.respondWith(request.text().then(function(body) {
-        var requestParameters = {
-          method: request.method,
-          path: url.slice(22),
-          body: body
-        }
-        return requestFromClient(event.clientId, requestParameters).then(function(response) {
-          return new Response(response.body, {status: response.status, statusText: response.statusText})
+        return requestFromClient(event.clientId, {path: path, body: body}).then(function(response) {
+          var headers = new Headers();
+          headers.append('Content-Type', response.contentType);
+          return new Response(response.body, {status: response.status, statusText: response.statusText, headers: headers})
         })
       }))
     } else {
