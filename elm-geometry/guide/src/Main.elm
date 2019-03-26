20137db0f5e3621c53854f4d8966815f9ea5ad67
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-03-26 19:52:18 -0400

    Remove testing code

--- elm-geometry/guide/src/Main.elm
+++ elm-geometry/guide/src/Main.elm
@@ -1,39 +1,6 @@
 module Main exposing (main)
 
-import Element exposing (Element)
-import Element.Events as Events
-import Element.Input as Input
 import Guide
-import Guide.Widget as Widget exposing (Widget)
-
-
-type CounterMsg
-    = Increment
-    | Decrement
-
-
-counter : Widget
-counter =
-    Widget.interactive
-        { init = 5
-        , view =
-            \count ->
-                Element.row [ Element.width Element.fill ]
-                    [ Element.row [ Element.centerX ]
-                        [ Input.button [] { onPress = Just Increment, label = Element.text "+" }
-                        , Element.text (String.fromInt count)
-                        , Input.button [] { onPress = Just Decrement, label = Element.text "-" }
-                        ]
-                    ]
-        , update =
-            \message model ->
-                case message of
-                    Increment ->
-                        model + 1
-
-                    Decrement ->
-                        model - 1
-        }
 
 
 main : Guide.Program
